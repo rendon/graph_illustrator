cb493ac10feba3c2602c1d36a5de7bc0fc822b6a
Rafael Rendón Pablo <rafaelrendonpablo@gmail.com>
2014-07-13 11:18:01 -0500

    Reload button

--- src/Main.java
+++ src/Main.java
@@ -18,6 +18,7 @@ public class Main extends JFrame {
     private static final int MAX = 200;
     private final JButton pointerButton;
     private final JButton openButton;
+    private final JButton reloadButton;
     private final JButton saveButton;
     private final JButton exportSvgButton;
     private final JButton newNodeButton;
@@ -43,22 +44,24 @@ public class Main extends JFrame {
         log = System.out;
         plane = new Plane();
         JToolBar toolBar = new JToolBar();
-        openButton = new JButton(getImage("open"));
-        saveButton = new JButton(getImage("save"));
-        exportSvgButton = new JButton(getImage("svg"));
-        JButton quitButton = new JButton(getImage("quit"));
-        newNodeButton = new JButton(getImage("node"));
-        newEdgeButton = new JButton(getImage("edge"));
-        pointerButton = new JButton(getImage("pointer"));
-        eraserButton = new JButton(getImage("eraser"));
+        openButton          = new JButton(getImage("open"));
+        reloadButton        = new JButton(getImage("reload"));
+        saveButton          = new JButton(getImage("save"));
+        exportSvgButton     = new JButton(getImage("svg"));
+        JButton quitButton  = new JButton(getImage("quit"));
+        newNodeButton       = new JButton(getImage("node"));
+        newEdgeButton       = new JButton(getImage("edge"));
+        pointerButton       = new JButton(getImage("pointer"));
+        eraserButton        = new JButton(getImage("eraser"));
 
-        shapeCircleButton = new JButton(getImage("circle"));
-        shapeRectangleButton = new JButton(getImage("rectangle"));
-        shapeNoneButton = new JButton(getImage("none"));
+        shapeCircleButton   = new JButton(getImage("circle"));
+        shapeRectangleButton= new JButton(getImage("rectangle"));
+        shapeNoneButton     = new JButton(getImage("none"));
 
 
         ActionHandler actionHandler = new ActionHandler();
         openButton.addActionListener(actionHandler);
+        reloadButton.addActionListener(actionHandler);
         saveButton.addActionListener(actionHandler);
         exportSvgButton.addActionListener(actionHandler);
         newNodeButton.addActionListener(actionHandler);
@@ -79,6 +82,7 @@ public class Main extends JFrame {
 
         toolBar.add(openButton);
         toolBar.add(saveButton);
+        toolBar.add(reloadButton);
         toolBar.add(exportSvgButton);
         toolBar.add(pointerButton);
         toolBar.add(newNodeButton);
@@ -101,12 +105,20 @@ public class Main extends JFrame {
         return new ImageIcon(getClass().getResource("/" + name + ".png"));
     }
 
-    void run() throws Exception
+    void run()
     {
         InputStreamReader input = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(input);
 
-        readGraph(reader);
+        try {
+            readGraph(reader);
+        } catch (IOException ioe) {
+            JOptionPane.showMessageDialog(this,
+                    "There was  a problem while trying to read " +
+                    "the file.\nCheck if your file contains a "  +
+                    "valid input.",
+                    "Error", JOptionPane.ERROR_MESSAGE);
+        }
         plane.updateUI();
     }
 
@@ -117,95 +129,91 @@ public class Main extends JFrame {
         test.run();
     }
 
-    private void readGraph(BufferedReader reader) throws Exception
+    private void readGraph(BufferedReader reader) throws IOException
     {
         HashMap<Integer, Vertex> G = new HashMap<Integer, Vertex>();
         String line, u , v, label;
         int nodeId = 0;
         HashMap<String, Integer> ids = new HashMap<String, Integer>();
 
-        try {
-            int operation = 0;
-            while ((line = reader.readLine()) != null) {
-                if (line.equals("[EDGES]")) {
-                    operation = READ_EDGE_INFO;
-                    continue;
-                } else if (line.equals("[VERTICES]")) {
-                    operation = READ_VERTEX_INFO;
-                    continue;
+        int operation = 0;
+        while ((line = reader.readLine()) != null) {
+            if (line.equals("[EDGES]")) {
+                operation = READ_EDGE_INFO;
+                continue;
+            } else if (line.equals("[VERTICES]")) {
+                operation = READ_VERTEX_INFO;
+                continue;
+            }
+
+            if (operation == READ_EDGE_INFO) {
+                String[] tokens = line.split(",");
+                u = tokens[0];
+                v = tokens[1];
+                if (tokens.length == 3)
+                    label = tokens[2];
+                else
+                    label = "";
+
+                u = u.replace("\\n", "\n");
+                v = v.replace("\\n", "\n");
+                Point2D pos = null;
+                if (tokens.length == 5) {
+                    double x = Double.parseDouble(tokens[4]);
+                    double y = Double.parseDouble(tokens[5]);
+                    pos = new Point2D(x, y);
                 }
 
-                if (operation == READ_EDGE_INFO) {
-                    String[] tokens = line.split(",");
-                    u = tokens[0];
-                    v = tokens[1];
-                    if (tokens.length == 3)
-                        label = tokens[2];
-                    else
-                        label = "";
+                int uid;
+                if (ids.containsKey(u)) {
+                    uid = ids.get(u);
+                } else {
+                    uid = nodeId++;
+                    ids.put(u, uid);
+                    Vertex vu = new Vertex(uid, u, pos);
+                    G.put(uid, vu);
+                }
 
-                    u = u.replace("\\n", "\n");
-                    v = v.replace("\\n", "\n");
-                    Point2D pos = null;
-                    if (tokens.length == 5) {
-                        double x = Double.parseDouble(tokens[4]);
-                        double y = Double.parseDouble(tokens[5]);
-                        pos = new Point2D(x, y);
-                    }
+                int vid;
+                if (ids.containsKey(v)) {
+                    vid = ids.get(v);
+                } else {
+                    vid = nodeId++;
+                    ids.put(v, vid);
+                    Vertex vv = new Vertex(vid, v, pos);
+                    G.put(vid, vv);
+                }
 
-                    int uid;
-                    if (ids.containsKey(u)) {
-                        uid = ids.get(u);
-                    } else {
-                        uid = nodeId++;
-                        ids.put(u, uid);
-                        Vertex vu = new Vertex(uid, u, pos);
-                        G.put(uid, vu);
-                    }
+                G.get(uid).addNeighbor(vid, label);
+                //G.get(vid).addNeighbor(uid, label);
+            } else if (operation ==  READ_VERTEX_INFO) {
+                String[] tokens = line.split(":|,");
 
-                    int vid;
-                    if (ids.containsKey(v)) {
-                        vid = ids.get(v);
-                    } else {
-                        vid = nodeId++;
-                        ids.put(v, vid);
-                        Vertex vv = new Vertex(vid, v, pos);
-                        G.put(vid, vv);
-                    }
+                u = tokens[0];
+                u = u.replace("\\n", "\n");
+                //String[] coordinates = tokens[1].split(",");
+                double x = Double.parseDouble(tokens[1]);
+                double y = Double.parseDouble(tokens[2]);
+                int align = StyleConstants.ALIGN_LEFT;
+                if (tokens.length == 4) {
+                    if (tokens[3].equals("C"))
+                        align = StyleConstants.ALIGN_CENTER;
+                    else if (tokens[3].equals("R"))
+                        align = StyleConstants.ALIGN_RIGHT;
+                }
 
-                    G.get(uid).addNeighbor(vid, label);
-                    //G.get(vid).addNeighbor(uid, label);
-                } else if (operation ==  READ_VERTEX_INFO) {
-                    String[] tokens = line.split(":|,");
-
-                    u = tokens[0];
-                    u = u.replace("\\n", "\n");
-                    //String[] coordinates = tokens[1].split(",");
-                    double x = Double.parseDouble(tokens[1]);
-                    double y = Double.parseDouble(tokens[2]);
-                    int align = StyleConstants.ALIGN_LEFT;
-                    if (tokens.length == 4) {
-                        if (tokens[3].equals("C"))
-                            align = StyleConstants.ALIGN_CENTER;
-                        else if (tokens[3].equals("R"))
-                            align = StyleConstants.ALIGN_RIGHT;
-                    }
-
-                    if (ids.containsKey(u)) {
-                        int id = ids.get(u);
-                        G.get(id).setCenter(new Point2D(x, y));
-                        G.get(id).setLabelAlignment(align);
-                    } else{
-                        Vertex vertex = new Vertex(nodeId, u, new Point2D(x,y));
-                        vertex.setLabelAlignment(align);
-                        ids.put(u, nodeId);
-                        G.put(nodeId, vertex);
-                        nodeId++;
-                    }
+                if (ids.containsKey(u)) {
+                    int id = ids.get(u);
+                    G.get(id).setCenter(new Point2D(x, y));
+                    G.get(id).setLabelAlignment(align);
+                } else{
+                    Vertex vertex = new Vertex(nodeId, u, new Point2D(x,y));
+                    vertex.setLabelAlignment(align);
+                    ids.put(u, nodeId);
+                    G.put(nodeId, vertex);
+                    nodeId++;
                 }
             }
-        } catch (Exception ioe) {
-            throw new Exception();
         }
 
         plane.setGraph(G);
@@ -235,7 +243,7 @@ public class Main extends JFrame {
                         readGraph(reader);
                         fis.close();
                         fileName = file.getAbsolutePath();
-                    } catch (Exception ioe) {
+                    } catch (IOException ioe) {
                         JOptionPane.showMessageDialog(null,
                                 "There was  a problem while trying to read "
                                 + "the file.\nCheck if your file contains a "
@@ -278,6 +286,19 @@ public class Main extends JFrame {
                 } else {
                     try {
                         saveToFile();
+                    } catch (IOException ioe) {
+                        ioe.printStackTrace();
+                    }
+                }
+            } else if (source == reloadButton) {
+                if (fileName != null) {
+                    try {
+                        InputStream fis = new FileInputStream(new File(fileName));
+                        InputStreamReader in = new InputStreamReader(fis);
+                        BufferedReader reader = new BufferedReader(in);
+                        readGraph(reader);
+                        fis.close();
+                        plane.updateUI();
                     } catch (IOException ioe) {
                         ioe.printStackTrace();
                     }
